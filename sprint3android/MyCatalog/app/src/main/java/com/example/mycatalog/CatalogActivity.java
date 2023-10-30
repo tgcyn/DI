78aa3d8af65a423747c1c51d17e1bb2c1712003b
Cynthia Tieso <ctieso22@fpcoruna.afundacion.org>
2023-10-30 17:51:02 +0100

    (S3.E2): crear un button y ligarlo a una actividad

--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -2,13 +2,31 @@ package com.example.mycatalog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CatalogActivity extends AppCompatActivity {
-
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+        //EJERCICIO 2
+        Button b1 = findViewById(R.id.primerButton); //creo una instancia button para ligarlo con el escrito en el xml
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { //este metodo indica q, cuando se pulse el boto, haga la accion especificada
+                Intent intent = new Intent(context, DetailActivity.class); //intent permite abrir la segunda actividad, tengo q dar dos parámetros: 1)la clase en la q estoy (context) 2)la clase q quiero iniciar (DetailActivity) espsdificada como clase (.class)
+                startActivity(intent); //esto hace q empiece la actividad
+            }
+        });
+
     }
 }
